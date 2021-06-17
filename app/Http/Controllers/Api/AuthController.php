35255e0c37a84cdf73e8b0acd48698e7b888019b
Debasis Chakraborty <chakraborty.debasis99@gmail.com>
2021-06-18 02:54:12 +0530

    Basic Refigter login function setup

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -7,5 +7,29 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    /**
+     * Register user API
+     * Author : Debasis Chakraborty
+     * Created On : 18 th June 2021
+     * Updated On : 18 th June 2021
+     * Last Update By : Debasis Chakraborty
+     */
+
+    public function register(Request $request)
+    {
+        # code...
+    }
+
+    /**
+     * Login user API
+     * Author : Debasis Chakraborty
+     * Created On : 18 th June 2021
+     * Updated On : 18 th June 2021
+     * Last Update By : Debasis Chakraborty
+     */
+
+    public function login(Request $request)
+    {
+        # code...
+    }
 }
